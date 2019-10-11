776829d908c3df21c095676e64c639c08cf14840
Your Name <you@example.com>
2019-10-11 09:44:20 +0800

    test

--- test.c
+++ test.c
@@ -2,6 +2,9 @@
 
 int main(void)
 {
-    printf("hellow,world\n");
+    printf("to C,or not to C:that is the question.\n");
+    printf("to c,or not to c:");
+    printf("that is the question.\n");
+    printf("brevity is the soul sf wit.\n--Shakespeare\n");
     return 0;
 }
